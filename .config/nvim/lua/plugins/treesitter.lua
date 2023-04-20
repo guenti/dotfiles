8127365d6f2d0b71be1f0bb6f859e6b9da614325
Mario Guenterberg <mario.guenterberg@googlemail.com>
2023-04-20 20:25:12 +0200

    Smali lang removed from treesitter
    Signed-off-by: Mario Guenterberg <mario.guenterberg@googlemail.com>

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
 	opts = {
 		ensure_installed = "all",
 		auto_install = true,
-		ignore_install = { "help", "swift", "wgsl", "wgsl_beavy" },
+		ignore_install = { "help", "swift", "wgsl", "wgsl_beavy", "smali" },
 		highlight = {
 			enable = true,
 			additional_vim_regex_hightlighting = false,
