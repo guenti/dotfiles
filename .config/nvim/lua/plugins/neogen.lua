76c3e2943c4c32ccb760b494a863e6fe16c2b6a7
Mario Guenterberg <mario.guenterberg@googlemail.com>
2023-05-23 18:56:17 +0200

    More backgrounds
    Signed-off-by: Mario Guenterberg <mario.guenterberg@googlemail.com>

--- .config/nvim/lua/plugins/neogen.lua
+++ .config/nvim/lua/plugins/neogen.lua
@@ -7,5 +7,6 @@ return {
 	init = function()
 		local opts = { noremap = true, silent = true }
 		vim.api.nvim_set_keymap("n", "<Leader>nf", ":lua require('neogen').generate()<CR>", opts)
+		vim.api.nvim_set_keymap("n", "<Leader>nc", ":lua require('neogen').generate({ type = 'class' })<CR>", opts)
 	end,
 }
